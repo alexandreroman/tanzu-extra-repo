fe9878025f124f538321b5b8a66620e9b14d9c48
Alexandre Roman <aroman@vmware.com>
2021-10-27 18:32:44 +0200

    Add kube-vip

--- hack/build-repo.sh
+++ hack/build-repo.sh
@@ -3,5 +3,6 @@ mkdir -p generated/packages
 mkdir -p generated/.imgpkg
 
 ytt -f packages/metallb > generated/packages/metallb.yaml
+ytt -f packages/kube-vip > generated/packages/kube-vip.yaml
 
 kbld -f generated/packages --imgpkg-lock-output generated/.imgpkg/images.yml
