0f2002f5bcd0f716118d08d86797e1787b2c16fe
Alexandre Roman <aroman@vmware.com>
2022-04-06 18:10:58 +0200

    Add Tekton 0.34.1

--- hack/build-repo.sh
+++ hack/build-repo.sh
@@ -4,5 +4,6 @@ mkdir -p generated/.imgpkg
 
 ytt -f packages/metallb > generated/packages/metallb.yaml
 ytt -f packages/kube-vip > generated/packages/kube-vip.yaml
+ytt -f packages/tekton > generated/packages/tekton.yaml
 
 kbld -f generated/packages --imgpkg-lock-output generated/.imgpkg/images.yml
