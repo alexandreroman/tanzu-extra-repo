db38ff48c481f3ba8104ce11bf7428aeb3d5fd6b
Alexandre Roman <aroman@vmware.com>
2022-04-07 10:51:37 +0200

    Rename package tekton to tekton-pipelines

--- hack/build-repo.sh
+++ hack/build-repo.sh
@@ -4,6 +4,6 @@ mkdir -p generated/.imgpkg
 
 ytt -f packages/metallb > generated/packages/metallb.yaml
 ytt -f packages/kube-vip > generated/packages/kube-vip.yaml
-ytt -f packages/tekton > generated/packages/tekton.yaml
+ytt -f packages/tekton-pipelines > generated/packages/tekton-pipelines.yaml
 
 kbld -f generated/packages --imgpkg-lock-output generated/.imgpkg/images.yml
