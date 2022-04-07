6a253a7c0997e09e00ff89338ddda4a5ed976a53
Alexandre Roman <aroman@vmware.com>
2022-04-07 12:16:26 +0200

    Add Tekton Dashboard 0.24.1

--- hack/build-repo.sh
+++ hack/build-repo.sh
@@ -5,5 +5,6 @@ mkdir -p generated/.imgpkg
 ytt -f packages/metallb > generated/packages/metallb.yaml
 ytt -f packages/kube-vip > generated/packages/kube-vip.yaml
 ytt -f packages/tekton-pipelines > generated/packages/tekton-pipelines.yaml
+ytt -f packages/tekton-dashboard > generated/packages/tekton-dashboard.yaml
 
 kbld -f generated/packages --imgpkg-lock-output generated/.imgpkg/images.yml
